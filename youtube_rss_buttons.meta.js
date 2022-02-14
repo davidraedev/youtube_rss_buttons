77eeb7d8c68030badf9b2dcdb2c912eecc6daa79
David Rae <daraeman@gmail.com>
2022-02-13 20:50:37 -0500

    update meta

--- youtube_rss_buttons.meta.js
+++ youtube_rss_buttons.meta.js
@@ -1,3 +1,11 @@
 // ==UserScript==
-// @version      1.2
+// @name         Youtube Rss Buttons
+// @namespace    youtube_rss_buttons
+// @description  Add RSS Buttons to Youtube
+// @homepageURL  https://github.com/daraeman/youtube_rss_buttons
+// @author       daraeman
+// @version      1.3
+// @date         2022-02-13
+// @downloadURL  https://github.com/daraeman/youtube_rss_buttons/raw/master/youtube_rss_buttons.user.js
+// @updateURL    https://github.com/daraeman/youtube_rss_buttons/raw/master/youtube_rss_buttons.meta.js
 // ==/UserScript==
