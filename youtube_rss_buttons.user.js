a2a03f53d7a546eff18bec76af388408078103af
David Rae <daraeman@gmail.com>
2022-02-13 20:55:11 -0500

    restrict urls to only those needed

--- youtube_rss_buttons.user.js
+++ youtube_rss_buttons.user.js
@@ -4,9 +4,11 @@
 // @description  Add RSS Buttons to Youtube
 // @homepageURL  https://github.com/daraeman/youtube_rss_buttons
 // @author       daraeman
-// @version      1.3
+// @version      1.3.1
 // @date         2022-02-13
-// @include      https://www.youtube.com/*
+// @include      https://www.youtube.com/channel/*
+// @include      https://www.youtube.com/c/*
+// @include      https://www.youtube.com/user/*
 // @downloadURL  https://github.com/daraeman/youtube_rss_buttons/raw/master/youtube_rss_buttons.user.js
 // @updateURL    https://github.com/daraeman/youtube_rss_buttons/raw/master/youtube_rss_buttons.meta.js
 // ==/UserScript==
