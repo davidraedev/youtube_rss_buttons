74a8f564a30d57d77b2ba27c266a72c1a7ff11c6
D <davidraedev@gmail.com>
2023-04-03 03:21:20 -0700

    Bump to version 1.3.3

--- youtube_rss_buttons.meta.js
+++ youtube_rss_buttons.meta.js
@@ -1,14 +1,3 @@
 // ==UserScript==
-// @name         Youtube Rss Buttons
-// @namespace    youtube_rss_buttons
-// @description  Add RSS Buttons to Youtube
-// @homepageURL  https://github.com/daraeman/youtube_rss_buttons
-// @author       daraeman
-// @version      1.3.2
-// @date         2022-02-13
-// @include      https://www.youtube.com/channel/*
-// @include      https://www.youtube.com/c/*
-// @include      https://www.youtube.com/user/*
-// @downloadURL  https://github.com/daraeman/youtube_rss_buttons/raw/master/youtube_rss_buttons.user.js
-// @updateURL    https://github.com/daraeman/youtube_rss_buttons/raw/master/youtube_rss_buttons.meta.js
+// @version      1.3.3
 // ==/UserScript==
